ae1bfdf77a853a8d15bf8fe11777a365527751e0
michbur <michalburdukiewicz@gmail.com>
2015-02-19 22:14:50 +0100

    preliminary analysis

--- sims_analysis.R
+++ sims_analysis.R
@@ -1,6 +1,19 @@
 length(three_n_power)
 
+#excluding third one, for some weird reason it has 5 n-grams on one positions, don't know why
+#check position_ngrams
+three_n_power <- three_n_power[-3]
 
-res_table <- do.call(rbind, lapply(three_n_power, function(single_rep)
-  unlist(lapply(single_rep, function(single_fraction) 
-    (sapply(single_fraction, length) - 1)/c(4, 32, 192)))))
+#real
+power <- Reduce("+", lapply(three_n_power, function(single_rep) {
+  #single replication dat
+  sr_dat <- do.call(rbind, lapply(single_rep, unlist))
+  sr_dat[, c(1, 3, 5)]/matrix(rep(c(4, 32, 192), 6), nrow = 6, byrow = TRUE)
+}))/length(three_n_power)
+
+
+lapply(three_n_power, function(single_rep) {
+  #single replication dat
+  sr_dat <- do.call(rbind, lapply(single_rep, unlist))
+  sr_dat[, 1]
+})[[3]]
