0c332d19e0662c28ab576775ad66a9f2162d11f5
michbur <michalburdukiewicz@gmail.com>
2015-02-19 22:15:17 +0100

    forgot to save again

--- sims_analysis.R
+++ sims_analysis.R
@@ -4,16 +4,20 @@ length(three_n_power)
 #check position_ngrams
 three_n_power <- three_n_power[-3]
 
-#real
+#true significant
 power <- Reduce("+", lapply(three_n_power, function(single_rep) {
   #single replication dat
   sr_dat <- do.call(rbind, lapply(single_rep, unlist))
   sr_dat[, c(1, 3, 5)]/matrix(rep(c(4, 32, 192), 6), nrow = 6, byrow = TRUE)
 }))/length(three_n_power)
 
-
-lapply(three_n_power, function(single_rep) {
+#false significant
+false <- Reduce("+", lapply(three_n_power, function(single_rep) {
   #single replication dat
   sr_dat <- do.call(rbind, lapply(single_rep, unlist))
-  sr_dat[, 1]
-})[[3]]
+  res <- (sr_dat[, c(2, 4, 6)] - sr_dat[, c(1, 3, 5)])/sr_dat[, c(2, 4, 6)]
+  #how to handle NaNs?situation, when we have 0 significant features?
+  #current klugde - treat them as 0.
+  res[is.nan(res)] <- 0
+  res
+}))/length(three_n_power)
